ca55e1317346eeecb0d8481414e5515f63497d66
Demfier <sahu.gaurav719@gmail.com>
2025-07-23 06:49:30 +0000

    Deploying to gh-pages from @ Demfier/demfier.github.io@cbd6f04d41f8141b94f249caf54d1f7e1b54d835 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,28 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%61%75%72%61%76[%64%6F%74]%73%61%68%75[%61%74]%6D%69%6C%61[%64%6F%74]%71%75%65%62%65%63", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://github.com/demfier", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=nMAt7UMAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/dem_fier", "_blank");
         },
       },{
         id: 'social-custom_social',
