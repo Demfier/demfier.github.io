1c4aa9ef1c0a50f98f41b92cb9abe8d66b75eed2
Demfier <sahu.gaurav719@gmail.com>
2025-07-24 00:23:58 +0000

    Deploying to gh-pages from @ Demfier/demfier.github.io@f68b03ea3b9fb1fddc109c43bfd5d0a86bd8afca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
